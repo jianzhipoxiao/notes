466d91ef488e3ebec7bb78df4777c2c297ffa388
小木蕊 <xiaomurui@163.com>
2024-04-09 15:06:35 +0800

    fix：二叉树众数

--- algorithms/src/main/java/cn/xiaomurui/note/algorithms/binaryTree/findMode/Solution.java
+++ algorithms/src/main/java/cn/xiaomurui/note/algorithms/binaryTree/findMode/Solution.java
@@ -3,33 +3,47 @@ package cn.xiaomurui.note.algorithms.binaryTree.findMode;
 import java.util.*;
 
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
+
+    int maxCount = 0;
+    int count = 0;
+    TreeNode pre;
+    List<Integer> result = new ArrayList<>();
 
     public int[] findMode(TreeNode root) {
         traversal(root);
-        int max = -1;
-        for (Integer key : map.keySet()) {
-            if (map.get(key) >= max) {
-                max = map.get(key);
-            }
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        for (Integer key : map.keySet()) {
-            if (map.get(key) == max) {
-                list.add(key);
-            }
-        }
-        int[] res = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            res[i] = list.get(i);
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(i);
         }
         return res;
     }
 
+    // 1.确定参数类型，返回值
     public void traversal(TreeNode root) {
+        // 2.递归结束条件
         if (root == null) return;
+        // 3.单层递归逻辑 中序遍历
         traversal(root.left);
-        map.put(root.val, map.getOrDefault(root.val, 0) + 1);
+        // 处理count计数值
+        if (pre == null)
+            count = 1;
+        else if (pre.val == root.val)
+            count++;
+        else
+            count = 1;
+
+        //更新pre 收割结果
+        pre = root;
+        if (count == maxCount) {
+            result.add(root.val);
+        }
+        // 更新maxCount result结果集
+        if (count > maxCount) {
+            maxCount = count;
+            result.clear();
+            result.add(root.val);
+        }
         traversal(root.right);
+        return;
     }
 }
